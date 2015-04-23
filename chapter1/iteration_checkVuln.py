79c74f35fa7876ece41a92465dbf19b7a3a4194a
Hoormazd Cyrus Kia <hckia1@gmail.com>
2015-04-22 18:22:44 -0700

    modified iteration_checkVuln.py

--- chapter1/iteration_checkVuln.py
+++ chapter1/iteration_checkVuln.py
@@ -10,20 +10,23 @@ def retBanner(ip, port):
 	except:
 		return
 def checkVulns(banner):
-	if '' in banner:
-		
-	elif '' in banner:
-		
-	elif '' in banner:
-		
-	elif '' in banner:
-		
+	if 'FreeFloat Ftp Server (Version 1.00)' in banner:
+		print '[+] FreeFloat FTP Server is vulnerable.'
+	elif '3Com 3CDaemon FTP Server Version 2.0' in banner:
+		print '[+] 3CDaemon FTP Server is vulnerable.'
+	elif 'Ability Server 2.34' in banner:
+		print '[+] Ability FTP Server is vulnerable.'
+	elif 'Sami FTP Server 2.0.2' in banner:
+		print '[+] Sami FTP Server is vulnerable.'
 	else:
-		
+		print '[-] FTP Server is not vulnerable.'
 	return
 def main():
 	portList = [21,22,25,80,110,443]
+	#ip = '208.113.192.172' <--- you can add this and comment out the for
+	#loop and ip value below.
 	for x in range(1, 255)
+		ip = '192.168.95.'+ str(x)
 		for port in portList:
 			banner = retBanner(ip, port)
 			if banner:
